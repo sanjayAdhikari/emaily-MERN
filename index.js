c1295a7296a6bdd61d04e619b4f6bb3eca6216ec
Sanjay Adhikari <idsanjay81@gmail.com>
2018-06-29 17:16:31 +0545

    change greeting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+    res.send({bye: 'Buddy'});
+}) 
 
 const PORT = process.env.PORT || 3000;
 
